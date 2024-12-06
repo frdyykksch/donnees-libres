920de422332d64e9fa439204918c57eb3bd268e4
javv <j4jaava@gmail.com>
2024-12-06 15:10:29 +0100

    ieornj

--- info111/cartes.cpp
+++ info111/cartes.cpp
@@ -6,10 +6,25 @@
 #include "tableau-lecture-csv.hpp"
 
 using namespace std;
+#include <SFML/Graphics.hpp>
 #include "lib/primitives.hpp"
 
-using namespace sf;
-
 int main() {
-	draw_point(w, {center.x + r * cos(t), center.y + r * sin(t)}, Color:: color);
+    // Crée une fenêtre de taille 640x480
+    RenderWindow window(VideoMode(640, 480), "Ma super fenêtre");
+
+    // Remplit la fenêtre de blanc
+    window.clear(Color::White);
+
+    // Affiche un point de couleur rouge
+    draw_point(window, {120, 5}, Color::Red);
+
+    // Actualise la fenêtre
+    window.display();
+
+    // Attend 10 secondes
+    sleep(seconds(10));
+
+    return 0;
 }
+
