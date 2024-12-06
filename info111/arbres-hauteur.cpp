ad7d5ddfafcbfad97469787083570f671242fb14
javv <j4jaava@gmail.com>
2024-12-06 14:43:02 +0100

    arbres

--- info111/arbres-hauteur.cpp
+++ info111/arbres-hauteur.cpp
@@ -19,13 +19,13 @@ int main() {
     double hauteur_max = 0.0;
     int index_max = 0;
     vector<string> C = colonne(t, 8);
-    vector<double> tableau_hauteur = conversionDouble(C);
+    vector<int> tableau_hauteur = conversionInt(C);
     for (int i = 0; i < tableau_hauteur.size();  i++) {
         if (tableau_hauteur[i] > hauteur_max) {
             hauteur_max = tableau_hauteur[i];
             index_max = i;
         }
     }
-    cout << "L'arbre le plus haut de Paris est un" << t[index_max][15] << "de" << hauteur_max << "mètres" << endl;
-    cout << "C'est un" << t[index_max][12] << ", du genre" << t[index_max][11] << endl;
+    cout << "L'arbre le plus haut de Paris est un " << t[index_max][15] << " de " << hauteur_max << " mètres" << endl;
+    cout << "C'est un " << t[index_max][12] << ", du genre " << t[index_max][11] << endl;
 }
