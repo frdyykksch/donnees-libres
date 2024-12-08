f464fd2d5fcd97f087817a5ad0250fb7693ec606
julien-cassou <juliencassou91@gmail.com>
2024-12-08 13:26:35 +0100

    huhu

--- info111/cartes.cpp
+++ info111/cartes.cpp
@@ -3,148 +3,98 @@
 #include "tableau-donnees.hpp"
 #include "tableau-donnees-avance.hpp"
 #include "tableau-lecture-csv.hpp"
-#include <SFML/Graphics.hpp>
-#include "lib/primitives.hpp"
-#include <cmath>
+// #include <SFML/Graphics.hpp>
+// #include "lib/primitives.hpp"
+// #include <cmath>
 using namespace std;
-using namespace sf;
+//using namespace sf;
 
-const double PI = 3.14159265359;
+// const double PI = 3.14159265359;
 
-void draw_circle(RenderWindow& window, Point center, int r, Color color) {
-    double dt = 1.0 / r;
-    for (double t = 0; t <= 2*PI; t += dt) {
-        draw_point(window, {center.x + r * cos(t), center.y + r * sin(t)}, color);
-    }
-}
-// CHAT GPT: separe les x des y
-vector<string> separerCoordonnees(string coordonnees) {
-    vector<string> resultat;
-    string temp = "";
-    
-    // Parcourir la chaîne
-    for(char c : coordonnees) {
-        if(c == ',') {
-            // Enlever les espaces et ajouter la première coordonnée
-            while(!temp.empty() && temp.back() == ' ') temp.pop_back();
-            resultat.push_back(temp);
-            temp = "";
-        } else {
-            // Ignorer les espaces au début
-            if(!(temp.empty() && c == ' ')) {
-                temp += c;
-            }
-        }
-    }
-    // Ajouter la deuxième coordonnée
-    while(!temp.empty() && temp.back() == ' ') temp.pop_back();
-    resultat.push_back(temp);
-    
-    return resultat;
-}
-// CHAT GPT SUITE
+// void draw_circle(RenderWindow& window, Point center, int r, Color color) {
+//     double dt = 1.0 / r;
+//     for (double t = 0; t <= 2*PI; t += dt) {
+//         draw_point(window, {center.x + r * cos(t), center.y + r * sin(t)}, color);
+//     }
+// }
 
-//Problème, ça créer un tableau sous la forme :
-// ["48.835092787824", "2.35346813513", "48.856614", "2.3522219"]
-// au lieu de :
-// [[x1, y1], [x2, y2], ...]
-
-vector<string> separerCoordonneesDansTableau(vector<string> tableau) {
-    vector<string> nouveauTableau;
-    
-    for(const string& coordonnees : tableau) {
-        // Séparer chaque coordonnée
-        string temp = "";
-        bool premiereCoordAjoutee = false;
-        
-        for(char c : coordonnees) {
-            if(c == ',') {
-                // Enlever les espaces et ajouter la première coordonnée
-                while(!temp.empty() && temp.back() == ' ') temp.pop_back();
-                nouveauTableau.push_back(temp);
-                temp = "";
-                premiereCoordAjoutee = true;
-            } else {
-                // Ignorer les espaces au début
-                if(!(temp.empty() && c == ' ')) {
-                    temp += c;
-                }
-            }
-        }
-        // Ajouter la deuxième coordonnée
-        while(!temp.empty() && temp.back() == ' ') temp.pop_back();
-        nouveauTableau.push_back(temp);
-    }
-    
-    return nouveauTableau;
-}
 int main() {
     // Lecture du fichier CSV
     vector<vector<string>> stations = litTableauCSV("donnees/velib-emplacement-des-stations.csv");
     
-    RenderWindow window(VideoMode(800, 600), "Carte des stations Vélib");
-    window.clear(Color::White);
-
+    // // Création de la fenêtre avec fond
+    // RenderWindow window(VideoMode(1200, 954), "Carte des stations Vélib");
+    // Texture texture;
+    // if (!texture.loadFromFile("donnees/Paris_plan.png")) {
+    //     cerr << "Erreur lors du chargement de l'image de fond" << endl;
+    //     return -1;
+    // }
+    // Sprite background(texture);
+    // window.draw(background);
 
     //création de deux tableaux qui contiennent de manière distinctes, la colone position et la colonne capacité de la station
     vector<string> position_str = colonne(stations, 3);
     vector<string> capacité_str = colonne(stations, 2);
     
     
-
-    
-}
-    vector<double> position = conversionDouble(separerCoordonnees(position_str));
+    vector<double> position = conversionDouble(position_str);
     vector<int> capacité = conversionInt(capacité_str);
-
-
-
-
-//// CHAT GPT ça
-
-//     // Trouver les bornes des coordonnées pour la mise à l'échelle
-//     double min_lat = stod(stations[0][2]), max_lat = stod(stations[0][2]);
-//     double min_lon = stod(stations[0][3]), max_lon = stod(stations[0][3]);
     
-//     for (const auto station : stations) {
-//         double lat = stod(station[2]);
-//         double lon = stod(station[3]);
-//         min_lat = min(min_lat, lat);
-//         max_lat = max(max_lat, lat);
-//         min_lon = min(min_lon, lon);
-//         max_lon = max(max_lon, lon);
-//     }
+    cout << "Positions brutes : " << endl;
+    for (const string& pos : position_str) {
+        cout << pos << endl;
+    }
+    
+    // Il faudrait parser la chaîne pour séparer latitude et longitude
+    vector<pair<double, double>> positions;
+    for (const string& pos : position_str) {
+        size_t virgule = pos.find(',');
+        if (virgule != string::npos) {
+            double lat = stod(pos.substr(0, virgule));
+            double lon = stod(pos.substr(virgule + 1));
+            positions.push_back({lat, lon});
+        }
+    }
+    
+    // Affichage des coordonnées parsées
+    cout << "\nPositions parsées (lat, lon) : " << endl;
+    for (const auto& pos : positions) {
+        cout << pos.first << ", " << pos.second << endl;
+    }
+
+    // //Affichage des positions
+    // for (const double& pos : position) {
+    //     cout << pos << " ";
+    // }
+    // cout << endl;
+}
 
 
+// test pour la création d'un vecteur qui sépare les coordonnées
 
+// int main() {
+//     int n = 0;
+//     vector<string> colonne = {"2.90930 , 8.9038403", "2.39403 , 8.39203"};
+//     vector<string> t = {""}; // Initialiser un vecteur de chaînes
 
-
-
-//     // Dessiner chaque station
-//     for (const auto& station : stations) {
-//         double lat = stod(station[2]);
-//         double lon = stod(station[3]);
-//         int capacity = stoi(station[4]);
-        
-//         // Conversion des coordonnées géographiques en pixels
-//         int x = (lon - min_lon) * 800 / (max_lon - min_lon);
-//         int y = 600 - (lat - min_lat) * 600 / (max_lat - min_lat);
-        
-//         // Rayon proportionnel à la capacité (ajuster le facteur selon besoin)
-//         int radius = 1 + capacity / 10;
-        
-//         draw_circle(window, {x, y}, radius, Color::Blue);
-//     }
-
-//     window.display();
-
-//     Event event;
-//     while (window.isOpen()) {
-//         while (window.pollEvent(event)) {
-//             if (event.type == Event::Closed)
-//                 window.close();
+//     for (auto valeur : colonne) {
+//         for (char value : valeur) {
+//             if (value != ',') {
+//                 t[n] += value;
+//             } else {
+//                 n += 1;
+//                 t.push_back(""); // Préparer une nouvelle entrée
+//             }
 //         }
 //     }
 
-//     return 0;
-// }
+//     for (auto valeiir : t) {
+//         cout << valeiir << " ";
+//     }
+//     cout << endl;
+
+//     return 0; // Indiquer que le programme s'est bien terminé
+
+
+
+
