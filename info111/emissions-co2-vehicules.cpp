1c8ca2b266c43d45f02393cb410ff6b4d44ec223
javv <j4jaava@gmail.com>
2024-12-08 14:19:24 +0100

    ex11

--- info111/emissions-co2-vehicules.cpp
+++ info111/emissions-co2-vehicules.cpp
@@ -21,18 +21,30 @@ int maxIndex(vector<T> tab) {
         } return maxIndexi;
 }
 
-int minIndex(vector<T> tab) {
+int minIndex(vector<double> tab) {
         int minIndexi = 0;
+        double minElement = tab[0];
+        for(int i = 0; i < tab.size(); i++) {
+                if(tab[i] < minElement && !tab[i] == 0) {
+                        minElement = tab[i];
+                        minIndexi = i;
+                }
+        } return minIndexi;
 }
 
 int main() {
         vector<vector<string>> csvVoitures = litTableauCSV("donnees/emissions-co2-vehicules-mars-2014.csv", 25);
         vector<string> consoMixte = colonne(csvVoitures, 13);
         vector<double> doubleConso = conversionDouble(consoMixte);
-
+        
         int maxConsoIndex = maxIndex(doubleConso);
+        int minConsoIndex = minIndex(doubleConso);
         
         // cerr << maxConsoIndex << endl;
         cout << "La voiture polluant le plus (2014) est une " << csvVoitures[maxConsoIndex][0] << " " << csvVoitures[maxConsoIndex][3] << endl;
         cout << "Elle consomme environ " << csvVoitures[maxConsoIndex][13] << "L/100km" << endl;
+
+        // cerr << minConsoIndex << endl;
+        cout << "La voiture polluant le moins (2014) est une " << csvVoitures[minConsoIndex][0] << " " << csvVoitures[minConsoIndex][3] << endl;
+        cout << "Elle consomme environ " << csvVoitures[minConsoIndex][13] << " L/100km" << endl;
 }
