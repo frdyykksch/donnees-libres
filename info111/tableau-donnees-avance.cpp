cec10372b7eb8978e5ec6b6a8219ffe1a9d3f270
javv <j4jaava@gmail.com>
2024-12-09 21:05:47 +0100

    commiti

--- info111/tableau-donnees-avance.cpp
+++ info111/tableau-donnees-avance.cpp
@@ -8,6 +8,10 @@
 
         // Frederik Kockisch
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs 
+*/ 
 int chercheIndice(vector<string> t, string valeur) {
     for(int i = 0; i < t.size(); i++) {
         if(t[i] == valeur) {
@@ -16,7 +20,15 @@ int chercheIndice(vector<string> t, string valeur) {
     } return -1;
 }
 
-// ajouté
+/* FONCTION AJOUTÉE
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs 
+*/ 
+/* Fonctions inVector
+ * @param t un tableau de valeur string
+ * @param s un string
+ * @return true, si la valeur s est dans le tableau t, false sinon
+*/ 
 bool inVec(vector<string> t, string s) {
     for(int i = 0; i < t.size(); i++) {
         if(t[i] == s) {
@@ -25,7 +37,11 @@ bool inVec(vector<string> t, string s) {
     } return false;
 }
 
-vector<string> distinct(vector<vector<string>> data, int j) { // check
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 90% - Pas complètement testé 
+*/ 
+vector<string> distinct(vector<vector<string>> data, int j) {
     vector<string> valeursDistinctes;
     for(int i = 0; i < data.size(); i++) {
         for(int k = 0; k < data[i].size(); k++) {
@@ -38,20 +54,22 @@ vector<string> distinct(vector<vector<string>> data, int j) { // check
     } return valeursDistinctes;
 }
 
-// ajouté
-int conversionSingleInt(string t) {
-    int resultat;
-    istringstream(t) >> resultat;
-    return resultat;
-}
-
-// ajouté
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 90% - Pas complètement testé 
+ * converti un string en double
+*/ 
 int conversionSingleDouble(string t) {
     double resultat;
     istringstream(t) >> resultat;
     return resultat;
 }
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 90% - Pas complètement testé 
+ * converti un string en double
+*/ 
 vector<double> conversionDouble(vector<string> t) {
     vector<double> resultat(t.size());
     for(int i = 0; i < t.size(); i++) {
@@ -60,13 +78,17 @@ vector<double> conversionDouble(vector<string> t) {
     return resultat;
 }
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs
+*/ 
 vector<int> groupByInt(vector<vector<string>> data, vector<string> valeurs, int j1, int j2) {
     vector<int> intStrValues(valeurs.size());
     for(int i = 0; i < data.size(); i++) {
         for(int j = 0; j < data[i].size(); j++) {
             if(j == j2 && inVec(valeurs,data[i][j1]))
                 // cerr << data[i][j2] << " - "; // db
-                intStrValues[chercheIndice(valeurs,data[i][j1])] += conversionSingleInt(data[i][j2]);
+                intStrValues[chercheIndice(valeurs,data[i][j1])] += stoi(data[i][j2]);
                 // cerr << intStrValues[chercheIndice(valeurs,data[i][1])] << " ";
         }
     } 
@@ -74,6 +96,10 @@ vector<int> groupByInt(vector<vector<string>> data, vector<string> valeurs, int 
     return intStrValues;
 }
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs
+*/ 
 vector<double> groupByDouble(vector<vector<string>> data, vector<string> valeurs, int j1, int j2) {
     vector<double> doubStrValues(valeurs.size());
     for(int i = 0; i < data.size(); i++) {
@@ -88,6 +114,10 @@ vector<double> groupByDouble(vector<vector<string>> data, vector<string> valeurs
     return doubStrValues;
 }
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs
+*/ 
 template<class T>
 vector<T> conversion(vector<string> t) {
     vector<T> resultat(t.size());
@@ -97,6 +127,10 @@ vector<T> conversion(vector<string> t) {
     return resultat;
 }
 
+/* 
+ * Frederik Kockisch
+ * Degré de confiance - 100% - Pas de bugs
+*/ 
 template<class T>
 vector<T> groupBy(vector<vector<string>> data, vector<string> valeurs, int j1, int j2) {
     vector<T> strValues(valeurs.size(), T());
@@ -105,7 +139,7 @@ vector<T> groupBy(vector<vector<string>> data, vector<string> valeurs, int j1, i
             if(j == j2 && inVec(valeurs, data[i][j1])) {
                 int index = chercheIndice(valeurs, data[i][j1]);
                 if (is_same<T, int>::value) { // aide par stackoverflow - https://stackoverflow.com/questions/992471/how-to-query-ift-int-with-template-class
-                    strValues[index] += conversionSingleInt(data[i][j2]);
+                    strValues[index] += stoi(data[i][j2]);
                 } else if (is_same<T, double>::value) {
                     strValues[index] += conversionSingleDouble(data[i][j2]);
                 }
